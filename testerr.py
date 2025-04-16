bfc601d1ccd63aba4da4115cfd5650a15f194f98
Midge <111672735+MidgeBot@users.noreply.github.com>
2025-04-16 10:44:47 -0700

    Update testerr.py
    did stuff. added things. idk

--- testerr.py
+++ testerr.py
@@ -5,6 +5,8 @@ root = tk.Tk()
 root.title("VariVirus")
 root.geometry("1200x800")
 
+ticker = 0
+
 #title code
 titleFrame = tk.Frame(root, height = 50, bg = 'lightblue')
 titleFrame.pack(fill='x')
@@ -16,18 +18,18 @@ titleLabel.pack(pady = 10)
 mainFrame = tk.Frame(root)
 mainFrame.pack(fill = "both", expand = True)
 
-#left panel + button
+#left panel + buttons + clock
 leftPanel = tk.Frame(mainFrame, width = 150, bg = "lightgrey")
 leftPanel.pack(side = "left", fill = "y")
 
 button1Var = tk.StringVar(value = "Age group")
 button1Opt = ["young", "old"]
-button1 = tk.OptionMenu(leftPanel, button1Var, *button1Opt)
+button1 = tk.OptionMenu(leftPanel, button1Var, * button1Opt)
 button1.pack(pady = 10, padx = 10, fill = "x")
 
 button2Var = tk.StringVar(value = "Vaccinated")
 button2Opt = ["yes", "No"]
-button2 = tk.OptionMenu(leftPanel, button2Var, *button2Opt)
+button2 = tk.OptionMenu(leftPanel, button2Var, * button2Opt)
 button2.pack(pady = 10, padx = 10, fill = "x")
 
 button3 = tk.Button(leftPanel, text = "Start Sim", command = lambda: toggleSimulation())
@@ -36,6 +38,9 @@ button3.pack(pady = 10, padx = 10, fill = "x")
 button4 = tk.Button(leftPanel, text = "Reset Sim", command = lambda: resetSimulation())
 button4.pack(pady = 10, padx = 10, fill = "x")
 
+clockLabel = tk.Label(leftPanel, text = f"Day: {ticker}", bg = "black", font = ("Ariel", 12))
+clockLabel.pack(anchor = "center", padx = 10)
+
 #right panel
 rightPanel = tk.Frame(mainFrame, bg = "white", bd = 1, relief = "solid")
 rightPanel.pack(side = "right", fill = "both", expand = True)
@@ -43,17 +48,53 @@ rightPanel.pack(side = "right", fill = "both", expand = True)
 rightUpperBox = tk.Frame(rightPanel, bg = "white", bd = 1, relief = "solid")
 rightUpperBox.pack(fill = "both", expand = True, padx = 10, pady = (10, 5))
 
+rightUpperCanvas = tk.Canvas(rightUpperBox, bg = "white", height = 100)
+rightUpperCanvas.pack(fill = "both", expand = True)
+
 rightLowerBox = tk.Frame(rightPanel, height = 200, bg = "lightgrey", highlightbackground = "black", highlightthickness = 1)
 rightLowerBox.pack(fill = "x", padx = 10, pady = (0,10))
+
+'''
+#circle, just a test to see if i can even make this work :/ old and outdated, just here for the vibes
+circleRadius = 20
+circleY = 50
+circleLeftX = 20
+circle = rightUpperCanvas.create_oval(0, 0, 0, 0, fill = "green")
+
+circle = rightUpperCanvas.create_oval(circleLeftX - circleRadius, circleY - circleRadius, circleLeftX + circleRadius, circleY + circleRadius, fill = "green") #this is so awful i hate this
+
+def moveCircle():
+    global ticker
+    canvasWidth = rightUpperCanvas.winfo_width()
+    padding = 20
+    
+    if canvasWidth < 100:
+        canvasWidth = 1200
+    
+    if ticker % 2 == 0:
+        newX = padding #right?
+    else:
+        newX = canvasWidth - padding #left?
+    
+    rightUpperCanvas.coords(circle, newX - circleRadius, circleY - circleRadius, newX + circleRadius, circleY + circleRadius)
+'''
 
 #timeline shenanigans
 timelineCanvas = tk.Canvas(rightLowerBox, bg = "white", height = 200)
 timelineCanvas.pack(fill = "both", padx = 5, pady = 5)
 
-def timelineGrid():
-    gridSize = 50
-    for i in range(0, 1200, gridSize):
-        timelineCanvas.create_line(i, 0, i, 200, fill = "Lightgrey")
+def timelineGrid(event = None):
+    timelineCanvas.delete("all")
+    gridCount = 16
+
+    canvasWidth = timelineCanvas.winfo_width()
+    gridSpacing = canvasWidth / (gridCount - 1)
+
+    for i in range(gridCount):
+        x = i * gridSpacing
+        timelineCanvas.create_line(x, 0, x, 200, fill = "Lightgrey")
+
+timelineCanvas.bind("<Configure>", timelineGrid)
 
 timelineGrid()
 
@@ -62,20 +103,25 @@ timelineCount = 0
 lastPoint = None
 
 def timelineUpdate():
-    global timelineCount, lastPoint
+    global timelineCount, lastPoint, ticker
     if not timelineRunning:
         return
-    xPos = timelineCount * 50
+    xPos = ticker * 50
     if xPos >= 1200:
         return
-    yPos = random.randint(30, 170)
+    yPos = random.randint(30, 170) #CHANGE THIS WHEN WE GET ACTAUL NUMBERS IDK
     rDot = 4
     timelineCanvas.create_oval(xPos - rDot, yPos - rDot, xPos + rDot, yPos + rDot, fill = "red")
     if lastPoint is not None:
         timelineCanvas.create_line(lastPoint[0], lastPoint[1], xPos, yPos, fill = "blue", width = 2)
     lastPoint = (xPos, yPos)
-    timelineCount += 1
-    rightLowerBox.after(250, timelineUpdate)
+    ticker += 1
+    
+    #moveCircle()
+
+    clockLabel.config(text = f"Day: {ticker}")
+
+    rightLowerBox.after(500, timelineUpdate)
 
 def toggleSimulation():
     global timelineRunning, timelineCount, lastPoint
@@ -84,14 +130,16 @@ def toggleSimulation():
         button3.config(text = "Resume Sim")
     else:
         timelineRunning = True
-        button3.config(text = "Pause Sim")
+        button3.config(text = "Pause Sim") 
         timelineUpdate()
 
 def resetSimulation():
-    global timelineRunning, timelineCount, lastPoint
+    global timelineRunning, timelineCount, lastPoint, ticker
     timelineRunning = False
     timelineCount = 0
     lastPoint = None
+    ticker = 0
+    clockLabel.config(text = f"Day: {ticker}")
     timelineCanvas.delete("all")
     timelineGrid()
     button3.config(text = "Start Sim")
